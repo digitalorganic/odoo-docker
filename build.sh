3a7733bc94574a5be126ce940b153697bc51576b
SPP <songpon.ph@gmail.com>
2022-03-07 16:07:38 +0700

    save

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@
 
 # docker push digitalorganic/odoo:15.0-odbc
 # docker push digitalorganic/odoo:14.0-odbc
-
-for ODOO_VERSION in 12.0 13.0 14.0 15.0
+# for ODOO_VERSION in 12.0 13.0 14.0 15.0
+for ODOO_VERSION in 14.0 15.0
 do
 docker build -f Dockerfile.odoo.dremio . --build-arg ODOO_VERSION=$ODOO_VERSION -t digitalorganic/odoo:$ODOO_VERSION-odbc
 docker push digitalorganic/odoo:$ODOO_VERSION-odbc
