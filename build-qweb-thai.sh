ea602b25b331ba603ae8bb9332c1b5744049e64c
SPP <songpon.ph@gmail.com>
2023-07-19 13:25:24 +0700

    with qweb thai

---
+++ build-qweb-thai.sh
@@ -0,0 +1,11 @@
+
+
+
+# for ODOO_VERSION in 12.0 13.0 14.0 15.0
+for ODOO_VERSION in 16.0
+do
+docker build -f Dockerfile.qweb.thai . --build-arg ODOO_VERSION=$ODOO_VERSION -t digitalorganic/odoo:$ODOO_VERSION-qweb-thai
+docker push digitalorganic/odoo:$ODOO_VERSION-qweb-thai
+
+
+done
