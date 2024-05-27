2402695e2a6c05ed387b75f3ecef87b921568d04
SPP <songpon.ph@gmail.com>
2024-05-27 09:14:34 +0700

    SAVEPOINT

--- build-odbc.sh
+++ build-odbc.sh
@@ -6,9 +6,9 @@
 # docker push digitalorganic/odoo:15.0-odbc
 # docker push digitalorganic/odoo:14.0-odbc
 # for ODOO_VERSION in 12.0 13.0 14.0 15.0
-for ODOO_VERSION in 16.0
+for ODOO_VERSION in 17.0
 do
-docker build -f Dockerfile.odoo.dremio . --build-arg ODOO_VERSION=$ODOO_VERSION -t digitalorganic/odoo:$ODOO_VERSION-odbc
+docker build -f Dockerfile.odoo.dremio . --no-cache --build-arg ODOO_VERSION=$ODOO_VERSION -t digitalorganic/odoo:$ODOO_VERSION-odbc
 docker push digitalorganic/odoo:$ODOO_VERSION-odbc
 
 
--- build.sh
+++ build.sh
@@ -6,9 +6,13 @@
 # docker push digitalorganic/odoo:15.0-odbc
 # docker push digitalorganic/odoo:14.0-odbc
 # for ODOO_VERSION in 12.0 13.0 14.0 15.0
+
 for ODOO_VERSION in 16.0
 do
-docker build -f Dockerfile.odoo.dremio . --build-arg ODOO_VERSION=$ODOO_VERSION -t digitalorganic/odoo:$ODOO_VERSION-odbc
+
+docker pull odoo:$ODOO_VERSION
+
+docker build -f Dockerfile.odoo.dremio . --build-arg ODOO_VERSION=$ODOO_VERSION -t digitalorganic/odoo:$ODOO_VERSION-odbc --no-cache
 docker push digitalorganic/odoo:$ODOO_VERSION-odbc
 
 
